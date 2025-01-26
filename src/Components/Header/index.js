b52cac457d2197c3afd79b32faa9ae5b538a585a
anupsingh04 <anupsingh727716@gmail.com>
2025-01-26 14:02:11 +0530

    initial commit - started tracking

---
+++ src/Components/Header/index.js
@@ -0,0 +1,44 @@
+import { Link } from 'react-router-dom';
+import Logo from '../../assets/images/logo.webp';
+import CountryDropdown from '../CountryDropdown';
+import { IoSearch } from 'react-icons/io5';
+import Button from '@mui/material/Button';
+
+
+const Header = ()=>{
+    return(
+        <>
+            <div className="headerWrapper">
+                <div className="top-strip bg-blue">
+                    <div className="container">
+                        <p className="mb-0 mt-0 text-center">Due to the <b>COVID 19</b> epidemic, orders may be processed with a slight delay</p>
+                    </div>
+                </div>
+
+                <header className="header">
+                    <div className="container">
+                        <div className="row">
+                            <div className="logoWrapper d-flex align-items-center col-sm-2">
+                                <Link to="/"><img src={Logo} alt='logo' /></Link>
+                            </div>
+
+                            <div className='col-sm-10 d-flex align-items-center part2'>
+                                <CountryDropdown/>
+
+                                {/* Header search starts here */}
+                                    <div className='headerSerch ml-3 mr-3'>
+                                        <input type='text'/>
+                                        <Button><IoSearch/></Button>
+                                    </div>
+                                {/* Header search ends here */}
+
+                            </div>
+                        </div>
+                    </div>
+                </header>
+            </div>
+        </>
+    )
+}
+
+export default Header;
